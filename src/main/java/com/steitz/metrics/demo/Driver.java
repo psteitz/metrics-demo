759a9478e3e18fff65c3644c982154f601020eb7
Phil Steitz <phil.steitz@gmail.com>
2018-07-29 10:25:34 -0700

    Initial shell.

--- src/main/java/com/steitz/metrics/demo/Driver.java
+++ src/main/java/com/steitz/metrics/demo/Driver.java
@@ -1,11 +1,44 @@
 package com.steitz.metrics.demo;
 
+import static com.codahale.metrics.MetricRegistry.name;
+
+import java.util.Random;
+
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer.Context;
 
 public class Driver {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+    /** Metrics registry */
+    static final MetricRegistry metrics = new MetricRegistry();
 
+    /** Processing timer */
+    private static final com.codahale.metrics.Timer processingTimer = metrics
+        .timer(name(Driver.class, "Processing time"));
+
+    /** Error count */
+    private static final Meter errorMeter = metrics
+        .meter(name(Driver.class, "Error rate"));
+
+    private static final Random random = new Random();
+
+    public static void main(String[] args)
+        throws InterruptedException {
+        double errRate = 0.25;
+        long itCount = 0;
+        while (true) {
+            if (itCount % 100 == 0) {
+                errRate = random.nextDouble() / 4;
+            }
+            final Context context = processingTimer.time();
+            Thread.sleep(100 +
+                         Math.round(Math.abs(random.nextGaussian()) * 1000));
+            context.close();
+            if (random.nextDouble() < errRate) {
+                errorMeter.mark();
+            }
+            itCount++;
+        }
     }
-
 }
