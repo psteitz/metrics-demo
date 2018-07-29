cc7897b813697eeeaeab1de019b88a34cc311359
Phil Steitz <phil.steitz@gmail.com>
2018-07-29 13:33:32 -0700

    Wire in Riemann.

--- src/main/java/com/steitz/metrics/demo/Driver.java
+++ src/main/java/com/steitz/metrics/demo/Driver.java
@@ -2,16 +2,22 @@ package com.steitz.metrics.demo;
 
 import static com.codahale.metrics.MetricRegistry.name;
 
+import java.io.IOException;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import com.codahale.metrics.Meter;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.Timer.Context;
+import com.codahale.metrics.riemann.Riemann;
+import com.codahale.metrics.riemann.RiemannReporter;
+
+import io.riemann.riemann.client.RiemannClient;
 
 public class Driver {
 
     /** Metrics registry */
-    static final MetricRegistry metrics = new MetricRegistry();
+    private static final MetricRegistry metrics = new MetricRegistry();
 
     /** Processing timer */
     private static final com.codahale.metrics.Timer processingTimer = metrics
@@ -24,9 +30,18 @@ public class Driver {
     private static final Random random = new Random();
 
     public static void main(String[] args)
-        throws InterruptedException {
+        throws InterruptedException,
+        IOException {
         double errRate = 0.25;
         long itCount = 0;
+        final RiemannClient riemannClient = RiemannClient
+            .tcp("my.riemann.server", 5555);
+        final Riemann riemann = new Riemann(riemannClient);
+        final RiemannReporter reporter = RiemannReporter.forRegistry(metrics)
+            .useSeparator("|").withTtl(500f)
+            .convertDurationsTo(TimeUnit.MILLISECONDS)
+            .convertRatesTo(TimeUnit.SECONDS).build(riemann);
+
         while (true) {
             if (itCount % 100 == 0) {
                 errRate = random.nextDouble() / 4;
